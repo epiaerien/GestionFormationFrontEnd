61bfcc8321bfa05b020845e8bae187aa0d911e4e
Leopold <leopold.delavaissiere@gmail.com>
2022-12-13 15:20:04 +0100

    components Formateur, Payement et Utilisateur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { GestionUtilisateurComponent } from './components/gestion-utilisateur/gestion-utilisateur.component';
+import { ListeFormationsComponent } from './components/liste-formations/liste-formations.component';
+import { GestionFormateurComponent } from './components/gestion-formateur/gestion-formateur.component';
+import { GestionPayementComponent } from './components/gestion-payement/gestion-payement.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GestionUtilisateurComponent,
+    ListeFormationsComponent,
+    GestionFormateurComponent,
+    GestionPayementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gestion-formateur/gestion-formateur.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GestionFormateurComponent } from './gestion-formateur.component';
+
+describe('GestionFormateurComponent', () => {
+  let component: GestionFormateurComponent;
+  let fixture: ComponentFixture<GestionFormateurComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GestionFormateurComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GestionFormateurComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
