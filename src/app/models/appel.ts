e5e41579206c10ddde3dc0ca9c09bf4ed4b13c2a
Maureen <maureen.cobert@gmail.com>
2022-12-13 12:45:27 +0100

    model appel formateur prospect

--- src/app/models/appel.ts
+++ src/app/models/appel.ts
@@ -1,2 +1,10 @@
+import { Commercial } from "./commercial";
+import { Prospect } from "./prospect";
+
 export class Appel {
+    id!:number; 
+    date!:Date; 
+    commentaire!:string; 
+    prospect!:Prospect; 
+    commercial!:Commercial; 
 }
