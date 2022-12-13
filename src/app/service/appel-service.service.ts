92d0515b68556e37dccb970ef5ffac674d83c670
Maureen <maureen.cobert@gmail.com>
2022-12-13 15:03:46 +0100

    service appel formateur prospect

--- src/app/service/appel-service.service.ts
+++ src/app/service/appel-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Appel } from '../models/appel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppelServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Appel[]>('http://localhost:8025/api/appels')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Appel>(`http://localhost:8025/api/appels/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/appels/${id}`)
+  }
+
+  add(appels:Appel)
+  {
+    return this.http.post(`http://localhost:8025/api/appels`,appels)
+  }
 }
--- src/app/service/prospect-service.service.ts
+++ src/app/service/prospect-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Prospect } from '../models/prospect';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProspectServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Prospect[]>('http://localhost:8025/api/prospects')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Prospect>(`http://localhost:8025/api/prospects/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/prospects/${id}`)
+  }
+
+  add(prospects:Prospect)
+  {
+    return this.http.post(`http://localhost:8025/api/prospects`,prospects)
+  }
 }
