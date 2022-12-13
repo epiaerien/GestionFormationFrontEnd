0fb8dc9fa6554275e23ea17f246f43c6ff5f1b0a
Antoine A <audeon.antoine@gmail.com>
2022-12-13 15:05:56 +0100

    Service commercial, participant, role

--- src/app/service/commercial-service.service.ts
+++ src/app/service/commercial-service.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Commercial } from '../models/commercial';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommercialServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Commercial[]>('http://localhost:8025/api/commercials')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Commercial>(`http://localhost:8025/api/commercials/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/commercials/${id}`)
+  }
+
+  add(commercial:Commercial)
+  {
+    return this.http.post(`http://localhost:8025/api/commercials`,commercial)
+  }
 }
--- src/app/service/role-service.service.ts
+++ src/app/service/role-service.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Role } from '../models/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoleServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Role[]>('http://localhost:8025/api/roles')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Role>(`http://localhost:8025/api/roles/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/roles/${id}`)
+  }
+
+  add(role:Role)
+  {
+    return this.http.post(`http://localhost:8025/api/roles`,role)
+  }
 }
+
