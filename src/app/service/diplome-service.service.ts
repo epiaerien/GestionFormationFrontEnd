6b9c87b424ff3bfda2218bc94c5f66edda296988
Leopold <leopold.delavaissiere@gmail.com>
2022-12-13 15:05:18 +0100

    service Payement, Diplome et Transaction

--- src/app/service/diplome-service.service.ts
+++ src/app/service/diplome-service.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Diplome } from '../models/diplome';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiplomeServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Diplome[]>('http://localhost:8025/api/diplomes')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Diplome>(`http://localhost:8025/api/diplomes/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/diplomes/${id}`)
+  }
+
+  add(diplome:Diplome)
+  {
+    return this.http.post(`http://localhost:8025/api/diplomes`,diplome)
+  }
 }
