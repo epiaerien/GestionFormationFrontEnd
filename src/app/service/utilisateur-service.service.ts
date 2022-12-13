6c86ad8ccbaa448074234f1ad0f95add15127fd8
Maureen <maureen.cobert@gmail.com>
2022-12-13 14:57:32 +0100

    service utilisateur

--- src/app/service/utilisateur-service.service.ts
+++ src/app/service/utilisateur-service.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Utilisateurs } from '../models/utilisateurs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilisateurServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  selectAll()
+  { 
+    return this.http.get<Utilisateurs[]>('http://localhost:8025/api/utilisateurs')
+  }
+
+  selectById(id:number)
+  {
+    return this.http.get<Utilisateurs>(`http://localhost:8025/api/utilisateurs/${id}`)
+  }
+
+  delete(id:number)
+  {
+    return this.http.delete(`http://localhost:8025/api/utilisateurs/${id}`)
+  }
+
+  add(utilisateurs:Utilisateurs)
+  {
+    return this.http.post(`http://localhost:8025/api/utilisateurs`,utilisateurs)
+  }
+
+
+
 }
