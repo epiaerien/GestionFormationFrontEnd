98dfd91de9fa328b3686027b2cb62723533f8578
Leopold <leopold.delavaissiere@gmail.com>
2022-12-16 16:43:14 +0100

    gestion utilisateur : suppr et modif + statu , gestion formation

--- src/app/components/liste-formations/liste-formations.component.ts
+++ src/app/components/liste-formations/liste-formations.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Formation } from 'src/app/models/formation';
 import { FormateurServiceService } from 'src/app/service/formateur-service.service';
 import { Router } from '@angular/router';
 import { FormationServiceService } from 'src/app/service/formation-service.service';
 import { Formateur } from 'src/app/models/formateur';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-liste-formations',
@@ -14,17 +15,25 @@ export class ListeFormationsComponent implements OnInit {
 
   formation!:Formation; 
   formations!:Formation[]; 
+  
   formateur!:Formateur;
+  change!:boolean
   
 
   constructor(private forService:FormationServiceService, private router:Router)
   {
 
   }
+
   ngOnInit(): void {
-this.afficherAll();
-this.formateur=new Formateur(); ;
-  }
+
+  this.afficherAll();
+  this.formateur=new Formateur();
+  this.formation = new Formation()
+  this.change = false
+  console.log("onInit" + this.change)
+  
+}
 
 
   afficherAll()
@@ -50,9 +59,30 @@ this.formateur=new Formateur(); ;
     this.router.navigateByUrl(`afficherParticipantParFormation/${id}`);
   }
 
+  modifier(id:number){
+  
+    this.forService.selectById(id).subscribe(
+
+    response => this.formation = response
+  )
+
+  }
+  delete(id:number){
+    
+  }
+
 
   formateurparid(id:number)
   {
     this.router.navigateByUrl(`afficherinfoformateur/${id}`); 
   }
+
+  onSubmitChange(bool:string){
+  
+    if(bool=="refresh"){
+    this.afficherAll()}
+
+
+  }
+
 }
