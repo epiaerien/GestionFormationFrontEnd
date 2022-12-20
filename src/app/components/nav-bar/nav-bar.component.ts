8a8ea455a2acd2ac860f1721d0dc1cfd0ace397b
Maureen <maureen.cobert@gmail.com>
2022-12-20 12:13:33 +0100

    bouton deconnexion

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Utilisateurs } from 'src/app/models/utilisateurs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+  utilisateur!:Utilisateurs
+
+  constructor(private route:Router){}
+  
+  ngOnInit(): void {
+
+    this.getUser()
+    
+  }
+
+  logout()
+  {
+    sessionStorage.clear()
+    this.route.navigateByUrl('login')
+  }
+
+  getUser(){
+    
+   if(sessionStorage.getItem("userDetails")){
+      this.utilisateur = JSON.parse(sessionStorage.getItem("userDetails") ?? "")
+    }
+ 
+    
+  }
+
+  toLogIn(){
+    
+    this.route.navigateByUrl('login')
+  }
 
 }
